f3691959eca1d4f5681363d9776387625c8d8116
dandan224 <43757729+dandan224@users.noreply.github.com>
2019-08-26 15:30:52 -0700

    Update K Smallest_In_Unsorted_Array.py
    Find the kth largest element in an unsorted array. Note that it is the kth largest element in the sorted order, not the kth distinct element.
    
    For example,
    Given [3,2,1,5,6,4] and k = 2, return 5.

--- K Smallest_In_Unsorted_Array.py
+++ K Smallest_In_Unsorted_Array.py
@@ -1,3 +1,4 @@
+#solution #1: order the array, then get the first k element
 class Solution(object):
   def kSmallest(self, array, k):
     """
@@ -8,9 +9,30 @@ class Solution(object):
     import heapq
     if not array:
       return []
+    # O(n)
     heapq.heapify(array)
     res = []
     # assume k would greater than the size of array
+    #O(klogn)
     for i in range(min(k, len(array))):
       res.append(heapq.heappop(array))
     return res
+  
+ # time complexity: O(n + klogn)
+
+#solution 2:
+ # heapify the first k elements to form a max_heap of size = k   O(k)
+  
+ # iterate over the rest elements one by one, if the new element less the top, update  O((n - k)logk)
+def kSmallest(array, k):
+  if not array:
+    return array
+  if k >= len(array):
+    return array
+  res = [-elem for elem in array[0:k]]
+  heapq.heapify(res)
+  for i in range(k, len(array)):
+    if array[i] < -res[0]:
+      heapq.heappop(res)
+      heapq.heappush(res, -array[i])
+  return [-elem for elem in res]
